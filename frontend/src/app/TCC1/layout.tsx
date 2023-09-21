8fffbcac3c98d5ec4b3c48afd0f7f02203652da0
CaioCintra <caiocintra2000@gmail.com>
2023-09-20 23:04:11 -0300

    TCC-89: Autenticação feita porcamente

--- frontend/src/app/TCC1/layout.tsx
+++ frontend/src/app/TCC1/layout.tsx
@@ -1,9 +1,11 @@
+"use client";
 import MenuLateral from "@/components/MenuLateral";
 import "../globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { useEffect } from "react";
 const inter = Inter({ subsets: ["latin"] });
-
+let token: any;
 export const metadata: Metadata = {
   title: "Sistema TCC",
   description: "Sistema TCC",
@@ -14,10 +16,20 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  function verificarToken() {
+    useEffect(() => {
+      token = localStorage.getItem("token-login");
+      console.log(token);
+      if (!token) window.location.href = "/";
+    }, []);
+  }
+
+  verificarToken();
+
   return (
     <html lang="en">
-      <body className={inter.className}>
-      <MenuLateral>{children}</MenuLateral>
+      <body>
+        <MenuLateral>{children}</MenuLateral>
       </body>
     </html>
   );
