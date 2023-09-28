4e0acde39c52ce648c6afad30a8655944175af82
CaioCintra <caiocintra2000@gmail.com>
2023-09-27 21:18:05 -0300

    TCC-94: Criação de link autenticado

---
+++ frontend/src/app/aluno/layout.tsx
@@ -0,0 +1,63 @@
+"use client";
+import "../globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { AuthProvider } from "@/contexts/AuthContext";
+import React, { useEffect } from "react";
+import { getAPIClient } from "@/services/axios";
+import { useSearchParams } from "next/navigation";
+import axios from "axios";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Sistema TCC",
+  description: "Sistema TCC",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+  console.log(token);
+
+  if (!token) {
+    window.location.href = "/erro/404";
+    return null;
+  }
+
+  useEffect(() => {
+    const verifyToken = async () => {
+      try {
+        const apiUrl = `http://localhost:3333/alunoAuth/token/${token}`;
+        const response = await axios.get(apiUrl);
+        const verify = response.data;
+        if (!verify) {
+          window.location.href = "/erro/404";
+        }
+      } catch (error) {
+        console.error("Erro ao verificar token:", error);
+        window.location.href = "/erro/404";
+      }
+    };
+
+    verifyToken();
+  }, [token]);
+
+  const apiClient = getAPIClient();
+  try {
+    apiClient.get("/alunos");
+  } catch (error) {
+    console.error("Erro ao obter dados:", error);
+  }
+  return (
+    <AuthProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </AuthProvider>
+  );
+}
