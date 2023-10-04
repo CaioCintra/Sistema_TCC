97ee8304749853ca319e25a0cacbb1f84ec52c63
CaioCintra <caiocintra2000@gmail.com>
2023-10-03 22:21:40 -0300

    TCC-99: Tela error 404

--- frontend/src/app/erro/404/page.tsx
+++ frontend/src/app/erro/404/page.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+"use client"
+import React from "react";
+import { Box } from "@mui/material";
 
 const Erro404 = () => {
   return (
-    <div>
-      <h1>Erro 404 - Página não encontrada</h1>
-      <p>Desculpe, a página que você está tentando acessar não existe.</p>
-    </div>
+    <body className="bg-[var(--primary-color)] flex justify-center text-center">
+      <Box className="bg-[var(--background-color)] w-[40%] top-1/4 rounded-lg p-10 absolute t-1/2 shadow-lg shadow-black">
+        <p className="font-semibold text-[var(--third-color)] text-7xl">
+          ERROR
+        </p>
+        <p className="font-semibold text-[var(--third-color)] text-9xl mb-5">404</p>
+        <p className="font-semibold text-[var(--third-color)] text-4xl mb-10">
+          Página não encontrada
+        </p>
+        <p className="font-semibold text-[var(--third-color)] text-lg">
+          Desculpe, a página que você está tentando acessar não existe.
+        </p>
+      </Box>
+    </body>
   );
 };
 
